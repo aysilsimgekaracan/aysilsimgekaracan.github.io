4c695317e52ac5cf213dc20bb018ed481ee893ed
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2021-06-16 23:46:01 +0300

    continued to create header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
+import profilePicture from "./assets/img/profilePicture.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Çok yakında.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="HeaderContainer">
+          <p className="Name_p">Ayşıl Simge Karacan</p>
+          <p className="Profile_p">Profile</p>
+          <p className="Resume_p">Resume</p>
+          <p className="ContactMe_p">Contact Me</p>
+        </div>
+        <div className="HeaderInfo_div">
+          <img
+            className="ProfilePicture"
+            src={profilePicture}
+            alt={"Profile Picture"}
+          />
+          <div className="HeaderDetail">
+            <p className="HeaderEmoji">👩‍💻</p>
+            <hr className="HeaderDivider" />
+            <div className="HeaderDetailText">
+              <p className="HeaderDetailTitle">Who I am?</p>
+              <p className="HeaderDetailSubtitle">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                vehicula sed eros eget facilisis. Maecenas varius sapien ut
+                eleifend facilisis. Nam gravida est nisi, non pretium mauris
+                condimentum vitae. Mauris tempor quam leo, ut blandit risus
+                mollis sit amet. Nam leo erat, vehicula vel hendrerit nec,
+                ultrices quis tellus.
+              </p>
+            </div>
+          </div>
+        </div>
       </header>
+      <p>Çok yakında.</p>
     </div>
   );
 }
